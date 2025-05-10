a8e6dda1144f0024d8275f7449c5eec0e158ac85
David AÏDASSO <davidaidasso252@gmail.com>
2025-05-10 23:29:24 +0100

    ...prompt js

---
+++ index.js
@@ -0,0 +1,33 @@
+// // 
+
+// document.write('Hello World!<br>JavaScript is awesome.');
+
+// alert('Hello World!');
+// confirm('Hello World!');
+
+// var proceed = confirm('Do you want to proceed');
+
+// var proceed = true;
+
+//  var age = prompt('What ur age ?');
+//  document.write('You are ' + age + ' years old');
+
+// var x = '-50.6';
+// console.log(Number(x));
+
+// document.write(Number('10'));
+
+// var isRaining = true ;
+
+// if(isRaining) {
+//     alert("Don't forget umbrella");
+// }
+
+var num = Number(prompt('Enter a number greater than 10'));
+
+if (num <= 10) {
+   alert('Invalid input');
+}   
+else {
+    alert(num);
+}
