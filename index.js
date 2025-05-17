f8022cfbf328624dd83ded3128ddf16fc881ef21
David AÏDASSO <davidaidasso252@gmail.com>
2025-05-17 10:56:44 +0100

    object

--- index.js
+++ index.js
@@ -1,62 +1,20 @@
-// // 
-function estPalindrome(mot){
+// // alert("Hello world !")
 
-    //Inverser un mot
-    let motInverse = mot.split('').reverse().join('');
+// const s = 'Hello world';
 
-    return mot === motInverse;
-}
+// console.log(s.substring(0, 5).toUpperCase());
 
-function verifierPalindrome(){
-    let mot = prompt("Entrer un mot: ");
-    
-    if(estPalindrome(mot)){
-        alert(`"${mot}" est un palindrome`)
-    }
-    else{
-        alert(`"${mot}" est un palindrome`)
-    }
-}
-
-verifierPalindrome();
-
-
-// document.write('Hello World!<br>JavaScript is awesome.');
-
-// alert('Hello World!');
-// confirm('Hello World!');
-
-// var proceed = confirm('Do you want to proceed');
-
-// var proceed = true;
-
-//  var age = prompt('What ur age ?');
-//  document.write('You are ' + age + ' years old');
-
-// var x = '-50.6';
-// console.log(Number(x));
-
-// document.write(Number('10'));
-
-// var isRaining = true ;
-
-// if(isRaining) {
-//     alert("Don't forget umbrella");
+// const person = {
+//     firstName:'David',
+//     lastName:'Jonathan',
+//     age:22,
+//     state: 'student',
+//     hobbies: ['music', 'basketball', 'games'],
+//     adress: {
+//         street: 'Maria-Gleta en face de Les Premiers',
+//         city:'Calavi',
+//         state:'BN'
+//     }
 // }
 
-// var num = Number(prompt('Enter a number greater than 10'));
-
-// if (num <= 10) {
-//    alert('Invalid input');
-// }   
-// else {
-//     alert(num);
-// }
-
-// function reusableFunction(a, b){
-//     console.log(a+b);
-// }
-
-// reusableFunction(5, 2);
-
- 
+// console.log(person.hobbies[1]);
