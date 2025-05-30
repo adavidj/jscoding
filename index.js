44fbc76f41e4b167e4c775f22c7903f19ac0a4ac
David AÏDASSO <davidaidasso252@gmail.com>
2025-05-30 23:05:57 +0100

    jeu de choix

--- index.js
+++ index.js
@@ -1,20 +1,36 @@
-// // alert("Hello world !")
+// Déclaration des tableaux contenant les listes des mots proposés à l'utilisateur
+const listeMots = ["Cachalot", "Pétunia", "Serviette"]
+const listePhrases = ["Pas de panique !", "La vie, l'univers et le reste", "Merci pour le poisson"]
 
-// const s = 'Hello world';
+let score = 0
 
-// console.log(s.substring(0, 5).toUpperCase());
+// Déclaration de la variable contenant le choix de l'utilisateur
+let choix = prompt("Avec quelle liste désirez-vous jouer : 'mots' ou 'phrases' ?")
+// Tant que l'utilisateur n'a pas saisi "mots" ou "phrases", on lui redemande de saisir un choix
+while (choix !== "mots" && choix !== "phrases") {
+    choix = prompt("Avec quelle liste désirez-vous jouer : 'mots' ou 'phrases' ?")
+}
 
-// const person = {
-//     firstName:'David',
-//     lastName:'Jonathan',
-//     age:22,
-//     state: 'student',
-//     hobbies: ['music', 'basketball', 'games'],
-//     adress: {
-//         street: 'Maria-Gleta en face de Les Premiers',
-//         city:'Calavi',
-//         state:'BN'
-//     }
-// }
-
-// console.log(person.hobbies[1]);
+if (choix === "mots") {
+    // On parcourt le tableau des mots
+    for (let i = 0; i < listeMots.length; i++) {
+        // On demande à l'utilisateur de saisir le mot correspondant à l'indice i
+        let motUtilisateur = prompt("Entrez le mot : " + listeMots[i])
+        if (motUtilisateur === listeMots[i]) {
+            // Si le mot saisi par l'utilisateur est correct, on incrémente le score
+            score++
+        }
+    }
+    console.log("Votre score est de " + score + " sur " + listeMots.length)
+} else {
+    // On parcourt le tableau des phrases
+    for (let i = 0; i < listePhrases.length; i++) {
+        // On demande à l'utilisateur de saisir la phrase correspondant à l'indice i
+        let phraseUtilisateur = prompt("Entrez la phrase : " + listePhrases[i])
+        if (phraseUtilisateur === listePhrases[i]) {
+            // Si la phrase saisi par l'utilisateur est correct, on incrémente le score
+            score++
+        }
+    }
+    console.log("Votre score est de " + score + " sur " + listePhrases.length)
+}
